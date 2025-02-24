98ece86c8336ac377a3521de8ad17008039bcc86
gaevuha <gaevuha@gmail.com>
2025-02-24 20:06:29 +0200

    add section vegetables

--- js/menu.js
+++ js/menu.js
@@ -2,16 +2,13 @@
     const refs = {
         openModalBtn: document.querySelector('[data-menu-open]'),
         closeModalBtn: document.querySelector('[data-menu-close]'),
-        modal: document.querySelector('[data-menu]'),
-        overlay: document.querySelector('[data-overlay]')
+        modal: document.querySelector('[data-menu]')
     };
 
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
-    refs.overlay.addEventListener('click', toggleModal);
 
     function toggleModal() {
         refs.modal.classList.toggle('is-open');
-        refs.overlay.classList.toggle('is-open');
     }
 })();
